fa9196d82d23bacbc694782d00ea37781ef4531b
kiky <syrino77@gmail.com>
2019-02-06 15:49:06 +0700

    JSON workshop

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
 
-class App extends StatefulWidget{
+class App extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -8,30 +10,47 @@ class App extends StatefulWidget{
   }
 }
 
-class AppState extends State<App>{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-    home: Scaffold(
-      body: Text('This is Body'),
-      appBar: AppBar(
-        actions: <Widget>[
-          Icon(Icons.android),
-          Icon(Icons.apps),
-          Icon(Icons.arrow_drop_down_circle)
-        ],
-        leading: Icon(Icons.arrow_back),
-        title: Text('This is AppBar'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_circle_outline),
-        onPressed: () {
-          print('You Click Floating');
-        },
-      ),
-    ),
-  );
+class AppState extends State<App> {
+  
+  int counter = 0;
+  List<String> contents = ['https://via.placeholder.com/600/92c952'];
+
+  readJSON() async {
+
+    counter += 1; 
+    var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var jsonReceive = json.decode(response.body);
+    contents.add(jsonReceive['url']);
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView.builder(
+          itemCount: contents.length,
+          itemBuilder: (context, counter) {
+            return Image.network(contents[counter]);
+          },
+        ),
+        appBar: AppBar(
+          actions: <Widget>[
+            Icon(Icons.android),
+            Icon(Icons.apps),
+            Icon(Icons.arrow_drop_down_circle)
+          ],
+          leading: Icon(Icons.arrow_back),
+          title: Text('This is AppBar'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add_circle_outline),
+          onPressed: () {
+            setState(() {
+              readJSON();
+            });
+          },
+        ),
+      ),
+    );
+  }
 }
